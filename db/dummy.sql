5c10a55c0e2dfa2f8b4206edcfb8d85c3c435e0a
rupalikarande <34995222+rupalikarande@users.noreply.github.com>
2018-03-11 16:19:18 +0530

    Add files via upload

--- db/dummy.sql
+++ db/dummy.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 25, 2018 at 12:49 PM
+-- Generation Time: Mar 09, 2018 at 12:12 PM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.16
 
@@ -21,6 +21,35 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `dummy` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `dummy`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `msgtab`
+--
+
+CREATE TABLE IF NOT EXISTS `msgtab` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `Phoneno` varchar(255) NOT NULL,
+  `Message` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `msgtab`
+--
+
+INSERT INTO `msgtab` (`id`, `date`, `subject`, `Phoneno`, `Message`) VALUES
+(1, '2018-03-09', 'test', '4589126547', 'hieee...'),
+(2, '2018-03-09', 'edrtuygj', '4567891234', 'fghngb'),
+(3, '2018-03-09', 'edrtuygj', '4567891234', 'fghngb'),
+(4, '2018-02-13', 'fkjdkgj', '7894568945', 'check mail...'),
+(5, '2018-02-13', 'fkjdkgj', '7894568945', 'check mail...'),
+(6, '0000-00-00', '', '', ''),
+(7, '0000-00-00', 'for confirmation', '9584753124', 'confirm mail............'),
+(8, '0000-00-00', 'regarding final submission', '2356417856', 'check dates of submission.\r\n    thank you');
 
 -- --------------------------------------------------------
 
@@ -70,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `textdemo` (
   `City` varchar(50) NOT NULL,
   `Email` varchar(150) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `textdemo`
@@ -78,7 +107,11 @@ CREATE TABLE IF NOT EXISTS `textdemo` (
 
 INSERT INTO `textdemo` (`id`, `Name`, `Phone_No`, `DOB`, `Address`, `City`, `Email`) VALUES
 (1, 'rupali', '9561753446', '19/07/1994', 'satara', 'satara', 'rup@gmail.com'),
-(2, 'kiran', '9850351531', '14/01/1992', 'pune', 'pune', 'kiran@gmail.com');
+(2, 'kiran', '9850351531', '14/01/1992', 'pune', 'pune', 'kiran@gmail.com'),
+(4, 'nikhil k', '7452684123', '01/20/1990', 'canada', 'UK', 'nik@gmail.com'),
+(5, 'shrutika karande', '8865239564', '03/01/2018', 'canada', 'uk', 'vini@gmail.com'),
+(6, 'john', '9685457825', '01/11/2017', 'hadapsar', 'pune', 'john@outlook.com'),
+(7, 'john', '9685457825', '01/11/2017', 'hadapsar', 'pune', 'john@outlook.com');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
