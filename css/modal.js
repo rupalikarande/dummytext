4fb10b9078ca0173176ef0b33bc6b68b941c82a3
rupalikarande <34995222+rupalikarande@users.noreply.github.com>
2018-03-11 16:18:42 +0530

    Add files via upload

---
+++ css/modal.js
@@ -0,0 +1,21 @@
+
+
+
+$(document).ready(function(){
+    var $remaining = $('#remaining'),
+        $messages = $remaining.next();
+
+    $('#message').keyup(function(){
+        var chars = this.value.length,
+            messages = Math.ceil(chars / 160),
+            remaining = messages * 160 - (chars % (messages * 160) || messages * 160);
+
+        $remaining.text(remaining + ' characters remain');
+        $messages.text(messages + ' message');
+    });
+});
+
+
+
+
+
