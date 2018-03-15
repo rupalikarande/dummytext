119462b6e43d1595f6dafe5d3cbd43f2895aab4c
rupalikarande <34995222+rupalikarande@users.noreply.github.com>
2018-03-15 11:51:09 +0530

    doesn't working multi query function

--- form.php
+++ form.php
@@ -31,7 +31,7 @@ $Add="";
 $city="";
 $email="";
 $dob="";
-
+$status="";
  
   $name_a=$_POST['Name'];
   $phone=$_POST['Phone_No'];
@@ -39,16 +39,41 @@ $dob="";
   $city=$_POST['City'];
   $email=$_POST['Email'];
   $dob=$_POST['DOB'];
+  // $status=$_POST['status'];
 
- // prepare and bind
-  $stmt = $conn->prepare("INSERT INTO textdemo (Name,Phone_No,Address,City,Email,DOB) VALUES (?,?,?,?,?,?)");
-  $stmt->bind_param("ssssss",$name_a,$phone,$Add,$city,$email,$dob);
+  $query = "INSERT INTO textdemo (Name,Phone_No,Address,City,Email,DOB) VALUES ($name_a,$phone,$Add,$city,$email,$dob);"; /*  first query : Notice the 2 semicolons at the end ! */
+$query .= "select Phone_No, count(*) as status from textdemo GROUP BY Phone_No;"; /* Notice the dot before = and the 2 semicolons at the end ! */
 
-   $result=$stmt->execute();
-   echo "New records created successfully";
-     echo "$name_a";
+$query .= "SQL STATEMENTS;"; 
 
-    $stmt->close();
+ 
+ if (mysqli_multi_query($conn, $query)) {
+  do {
+      /* store first result set */
+      if ($result = mysqli_store_result($conn)) {
+          while ($row = mysqli_fetch_array($result)) 
+  
+  /* print your results */    
+  {
+  echo $row['Phone_NO'];
+  echo $row['City'];
+  }
+  mysqli_free_result($result);
+  }   
+  } while (mysqli_next_result($conn));
+  }
+
+
+  // $stmt = $conn->prepare("INSERT INTO textdemo (Name,Phone_No,Address,City,Email,DOB) VALUES (?,?,?,?,?,?)");
+  // $stmt->bind_param("ssssss",$name_a,$phone,$Add,$city,$email,$dob);
+
+  //  $result=$stmt->execute();
+  //  echo "New records created successfully";
+  //    echo "$name_a";
+  //    $stmt->close();
+
+   
+    // header("location:form.html");
     } 
     
   //  view Datababase
